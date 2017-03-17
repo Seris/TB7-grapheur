e86ed9f21e1f93de2fead29c79b7c6bd6ac28746
Thalya <seris@protonmail.ch>
2017-03-17 14:50:33 +0000

    blbl

--- main.c
+++ main.c
@@ -6,19 +6,6 @@
 #include "inc/test.h"
 
 int main(int argc, char* argv[]){
-    // menu(argc, argv);
-
-    tokenlist_t* list = create_test_list();
-    print_test_list(list);
-
-    err_t error;
-    tokenarb_t* arb = parse_token_list(list, &error);
-
-    if(arb != NULL){
-        print_tree(arb);
-    } else {
-        print_error("fonction de test", error);
-    }
-
+    menu(argc, argv);
     return EXIT_SUCCESS;
 }
