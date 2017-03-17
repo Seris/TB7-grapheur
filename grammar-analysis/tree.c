6ac7e9c17317d2ed3cbc96ccaadcc6bee73e8ea9
Thalya <Seris@users.noreply.github.com>
2017-03-17 09:51:37 +0100

    Update tree.c

--- grammar-analysis/tree.c
+++ grammar-analysis/tree.c
@@ -3,6 +3,10 @@
 #include "../inc/syntax-check.h"
 #include "../inc/types.h"
 
+/**
+ * Supprimer de la mémoire un arbre binaire de token
+ * @param  abr      L'arbre qui doit être free
+ */
 void free_arbre_token(tokenarb_t* abr){
     if(abr != NULL){
         free_arbre_token(abr->gauche);
