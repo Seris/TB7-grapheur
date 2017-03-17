1703eab870ef90c71c10e153d48e2f7499b19fbf
Thalya <seris@protonmail.ch>
2017-03-17 15:04:02 +0000

    blbl

--- main.c
+++ main.c
@@ -8,10 +8,20 @@
 int main(int argc, char* argv[]){
     // menu(argc, argv);
 
-    tokenarb_t a;
-    a.token.type = VAR;
+    tokenarb_t a1, a2, a3;
+    a1.token.type = VAR;
 
-    lancerGrapheur(argc, argv, &a, -5, 5, 0.05);
+    a2.token.type = OPERATEUR;
+    a2.token.valeur.operateur = '/';
+
+    a3.token.type = REEL;
+    a3.token.valeur.reel = 1;
+
+    a2.gauche = &a3;
+    a2.droite = &a1;
+
+
+    lancerGrapheur(argc, argv, &a2, -5, 5, 0.05);
 
     return EXIT_SUCCESS;
 }
